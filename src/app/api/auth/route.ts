1a3143b4d770463e2919d83703c5d05288da6490
Will <will00987654@gmail.com>
2025-10-20 15:09:11 -0300

    Ajuste de importação de paginas

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { prisma } from "@/lib/prisma";
+import { prisma } from "@/src/lib/prisma";
 
 // Adicione as novas senhas aqui
 const hardcodedPasswords: Record<string, number> = {
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { ThemeProvider } from "@/components/theme/theme-provider";
-import { Toaster } from "@/components/ui/toaster";
+import { ThemeProvider } from "@/src/components/theme/theme-provider";
+import { Toaster } from "@/src/components/ui/toaster";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
